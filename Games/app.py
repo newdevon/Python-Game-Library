f3b5e0e518ed5cb3f5920b66e8721c78d158c3d4
newdevon <stvchan999@gmail.com>
2022-08-05 20:01:45 -0400

    PGL-3: added personal reaction time game with polish

--- Games/app.py
+++ Games/app.py
@@ -1,5 +1,6 @@
 from rps import player_vs_computer, player_vs_player
 from ng import number_guesser
+from rt import reaction_time
 import time
 
 def app():
@@ -10,16 +11,17 @@ def app():
     
     while True:
         try:
-            game = int(input("(1) Rock Paper Scissor 1v1\n(2) Rock Paper Scissor vs Ai\n(3) Number Guessor\nYour choice? "))
+            game = int(input("(1) Rock Paper Scissor 1v1\n(2) Rock Paper Scissor vs Ai\n(3) Number Guessor\n(4) Reaction Time\nYour choice? "))
 
             if game == 1: player_vs_player()
-            if game == 2: player_vs_computer()
-            if game == 3: number_guesser()
+            elif game == 2: player_vs_computer()
+            elif game == 3: number_guesser()
+            elif game == 4: reaction_time()
 
         except ValueError:
             print("\nPlease enter a valid entry\n")
 
-        play_again = input("exit? (yes/no): ")
+        play_again = input("exit library? (yes/no): ")
         if play_again == "yes":
             print("Shutting down...")
             time.sleep(1)
